c76229bcd8c6ab88066e65403d64a3539a1a5806
Hayden Bruin <Hayden.Bruin@cucumber.co.nz>
2019-07-24 16:43:50 +1200

    setting up header scss

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,18 @@
 //import { Link } from "gatsby"
 import React from "react"
 
-const Header = () => <header>header</header>
+const Header = () => (
+  <div class="header">
+    <div class="header-logo">
+      <img class="header-logo-image" src="images/juniordev_logo.png" alt="logo"/>
+      <span class="header-logo-top">JuniorDev</span>
+      <span class="header-logo-bottom text-red">Tauranga</span>
+    </div>
+    <div class="header-navigation">
+      <a href="#changeme">Next event</a> / <a href="#changeme">Meet the hosts</a> /
+      <a href="#changeme">Slack channel</a> / <a href="#changeme">Newsletter</a>
+    </div>
+  </div>
+)
 
 export default Header
