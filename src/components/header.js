1595189f7a4a8f6d5e3508b07b3f58608d755908
EmeraldSlash <riorex18@gmail.com>
2019-07-24 18:11:19 +1200

    Fixed issues caused by gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,16 @@
 //import { Link } from "gatsby"
 import React from "react"
+import Image_JuniorDevLogo from "../images/juniordev_logo.png"
 
 const Header = () => (
   <div class="header">
     <div class="header-logo">
-      <img class="header-logo-image" src="images/juniordev_logo.png" alt="logo"/>
+      <img class="header-logo-image" src={Image_JuniorDevLogo} alt="logo"/>
       <span class="header-logo-top">JuniorDev</span>
       <span class="header-logo-bottom text-red">Tauranga</span>
     </div>
     <div class="header-navigation">
-      <a href="#changeme">Next event</a> / <a href="#changeme">Meet the hosts</a> /
-      <a href="#changeme">Slack channel</a> / <a href="#changeme">Newsletter</a>
+      <a href="#changeme">Next event</a> / <a href="#changeme">Meet the hosts</a> / <a href="#changeme">Slack channel</a> / <a href="#changeme">Newsletter</a>
     </div>
   </div>
 )
