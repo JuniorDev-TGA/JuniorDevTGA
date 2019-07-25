6344eb6aac1a5d6dadb042cb55b0ab490d3693fe
EmeraldSlash <riorex18@gmail.com>
2019-07-25 16:59:44 +1200

    Added basic CSS

--- src/components/hosts.js
+++ src/components/hosts.js
@@ -2,14 +2,13 @@
 import React from "react"
 
 // IMAGES
-import Pattern from "../images/hosts-pattern.jpg"
 import Kelly from "../images/kelly-williams.jpg"
 import Josh from "../images/josh.jpg"
 import Matias from "../images/matias.jpg"
-import Michael from "../images/michael.jpg"
+import Michael from "../images/michael-forde.jpg"
 import Hayden from "../images/hayden.jpg"
-import DatacomLogo from "../images/datacom-logo.jpg"
-import CucumberLogo from "../images/cucumber-logo.jpg"
+import DatacomLogo from "../images/datacom-logo.png"
+import CucumberLogo from "../images/cucumber-logo.png"
 
 const Hosts = () => (
   <div className="c-hosts">
@@ -17,33 +16,32 @@ const Hosts = () => (
       <h1>Meet the hosts</h1>
       <div className="c-hosts__people">
         <div className="c-hosts__person">
-          <img className="c-hosts__person-image" src={Josh}></img>
+          <img className="c-hosts__person-image" src={Josh} alt=""></img>
           <span className="c-hosts__person-name">Josh Biddick</span>
-          <img className="c-hosts__person-company" src={CucumberLogo}></img>
+          <img className="c-hosts__person-company" src={CucumberLogo} alt="Cucumber"></img>
         </div>
         <div className="c-hosts__person">
-          <img className="c-hosts__person-image" src={Kelly}></img>
+          <img className="c-hosts__person-image" src={Kelly} alt=""></img>
           <span className="c-hosts__person-name">Kelly Williams</span>
-          <img className="c-hosts__person-company" src={CucumberLogo}></img>
+          <img className="c-hosts__person-company" src={CucumberLogo} alt="Cucumber"></img>
         </div>
         <div className="c-hosts__person">
-          <img className="c-hosts__person-image" src={Michael}></img>
+          <img className="c-hosts__person-image" src={Michael} alt=""></img>
           <span className="c-hosts__person-name">Michael Ford</span>
-          <img className="c-hosts__person-company" src={DatacomLogo}></img>
+          <img className="c-hosts__person-company" src={DatacomLogo} alt="Datacom"></img>
         </div>
         <div className="c-hosts__person">
-          <img className="c-hosts__person-image" src={Matias}></img>
+          <img className="c-hosts__person-image" src={Matias} alt=""></img>
           <span className="c-hosts__person-name">Matias Iacono</span>
-          <img className="c-hosts__person-company" src={CucumberLogo}></img>
+          <img className="c-hosts__person-company" src={CucumberLogo} alt="Cucumber"></img>
         </div>
         <div className="c-hosts__person">
-          <img className="c-hosts__person-image" src={Hayden}></img>
+          <img className="c-hosts__person-image" src={Hayden} alt=""></img>
           <span className="c-hosts__person-name">Hayden Bruin</span>
-          <img className="c-hosts__person-company" src={CucumberLogo}></img>
+          <img className="c-hosts__person-company" src={CucumberLogo} alt="Cucumber"></img>
         </div>
       </div>
     </div>
-    <img src={Pattern} className="c-hosts__pattern" alt=""></img>
   </div>
 )
 
