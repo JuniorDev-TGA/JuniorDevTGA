aa6c1c99e430ac951fcb4dd6d0f0348f78171dc9
Hayden Bruin <Hayden.Bruin@cucumber.co.nz>
2019-09-26 11:07:48 +1200

    adding success/error messages to the newsletter component"

--- src/components/newsletter-form.js
+++ src/components/newsletter-form.js
@@ -1,21 +1,60 @@
 import React, { useState } from "react"
-import addToMailchimp from 'gatsby-plugin-mailchimp'
+import addToMailchimp from "gatsby-plugin-mailchimp"
+import { Hearts } from "../icons"
 
 const NewsletterForm = () => {
-  const [emailAddress, setEmailAddress] = useState('');
+  const [emailAddress, setEmailAddress] = useState("")
+  const [mailChimpResult, setMailChimpResult] = useState({})
+  const [isPendingMailChimp, setIsPendingMailChimp] = useState(false)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async e => {
+    e.preventDefault()
+    setIsPendingMailChimp(true)
     const result = await addToMailchimp(emailAddress)
-    console.log(result)
-    return
+    setIsPendingMailChimp(false)
+    setMailChimpResult(result)
   }
-  
+
+  if (mailChimpResult && mailChimpResult.result === "success") {
+    return (
+      <div className="c-newsletter__success">
+        Thank You! You've been signed up to our newsletter, you'll be hearing
+        from us soon!
+      </div>
+    )
+  }
+
   return (
-  <form className="c-newsletter__form" onSubmit={handleSubmit}>
-      <input type="email" value={emailAddress.value} className="c-newsletter__form-email c-input" placeholder="Enter your email address..." onChange={e => setEmailAddress(e.target.value)}></input>
-      <input type="submit" className="c-newsletter__form-submit c-button" value="Subscribe"></input>
-  </form>
+    <>
+      {mailChimpResult &&
+        mailChimpResult.result === "error" &&
+        !isPendingMailChimp && (
+          <div
+            className="c-newsletter__error"
+            dangerouslySetInnerHTML={{ __html: mailChimpResult.msg }}
+          ></div>
+        )}
+      {isPendingMailChimp ? (
+        <div className="c-newsletter__loading">
+          <Hearts />
+        </div>
+      ) : (
+        <form className="c-newsletter__form" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={emailAddress.value}
+            className="c-newsletter__form-email c-input"
+            placeholder="Enter your email address..."
+            onChange={e => setEmailAddress(e.target.value)}
+          ></input>
+          <input
+            type="submit"
+            className="c-newsletter__form-submit c-button"
+            value="Subscribe"
+          ></input>
+        </form>
+      )}
+    </>
   )
-} 
+}
 export default NewsletterForm
